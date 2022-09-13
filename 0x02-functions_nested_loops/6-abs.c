bba32f0f4a0a9a677b7256c033407ec7465300f2
malachymalachy <malachymalachy>
2022-09-13 15:13:29 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - Main function entry point
+ * @n : character to check the case
+ * Return: Always zero
+ */
+int _abs(int n) /* The function returns void */
+{
+	return (n >= 0 ? n : n * -1);
+}
