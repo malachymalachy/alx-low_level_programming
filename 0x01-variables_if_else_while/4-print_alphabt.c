69bcbd1b3c22edb7cf4dd57001a91deeb2fd581f
malachymalachy <malachymalachy>
2022-09-12 17:50:51 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,49 +1,25 @@
-/* Program to determine if a number is positive, negative or zero */                                                                  
+/* Program to determine if a random number is positive, negative or zero */
 
-                                                                                                                                      
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-#include <stdlib.h>                                                                                                                   
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main(void) /* The function returns void */
+{
+	char letter = 'a';
 
-#include <time.h>                                                                                                                     
-
-#include <stdio.h>                                                                                                                    
-
-                                                                                                                                      
-
-/**                                                                                                                                   
- *
- *  * main -Main function entry point                                                                                                    
- *
- *   * Return: Allways zero                                                                                                               
- *
- *    */                                                                                                                                   
-
-int main(void) /* The function returns void */                                                                                        
-
-{                                                                                                                                     
-
-	        char letter = 'a';                                                                                                            
-
-		                                                                                                                                      
-
-		        for (letter = 'a'; letter <= 'z'; letter++)                                                                                   
-
-				        {                                                                                                                             
-
-						                if (letter == 'q' || letter == 'e')                                                                                   
-
-									                {                                                                                                                     
-
-												                        continue;                                                                                                     
-
-															                }                                                                                                                     
-
-								                putchar(letter);                                                                                                      
-
-										        }                                                                                                                             
-
-			        putchar('\n');                                                                                                                
-
-				        return (0);                                                                                                                   
-
-
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
