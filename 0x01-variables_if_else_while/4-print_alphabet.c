68e2a40c62201da1ec46140af57f5ffc0482b11a
malachymalachy <malachymalachy>
2022-09-12 17:21:13 -0700

    When I was having that alphabet soup, I never thought it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+/* Program to determine if a number is positive, negative or zero */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Allways zero
+ */
+int main(void) /* The function returns void */
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
