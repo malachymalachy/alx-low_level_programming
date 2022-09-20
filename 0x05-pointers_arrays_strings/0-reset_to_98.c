c1c7c19720160cc073ff4dfd7e603208daec8f85
malachymalachy <malachymalachy>
2022-09-20 14:24:30 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - void reset_to_98 
+ * main - void reset_to_98
  *
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-	int *n;
+	int n;
 	int *p;
 
-	*n = 98;
+	n = 98;
 	p = &n;
+	printf("Address of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
 	return (0);
 }
