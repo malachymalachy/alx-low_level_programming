fbc56392e40c584faf6bdf7319a7d7390a46f8f9
malachymalachy <malachymalachy>
2022-09-20 14:28:00 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,11 +7,11 @@
  */
 void reset_to_98(int *n)
 {
-	int n;
+	int *n;
 	int *p;
 
-	n = 98;
-	p = &n;
+	*n = 98;
+	p = &*n;
 	printf("Address of 'n': %p\n", &n);
 	printf("Value of 'p': %p\n", p);
 	return (0);
